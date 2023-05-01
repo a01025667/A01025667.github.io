7a945259ff6d496fad62220312a3b17ef2b2ccfb
Cristina <A01025667@tec.mx>
2023-05-01 17:14:43 -0600

    Prueba

---
+++ JS/501_javascript.js
@@ -0,0 +1,20 @@
+// Escribe una función que encuentre el primer carácter de un cadena de texto que no se repite. Prueba tu función con: 'abacddbec'
+function norep(cad)
+{
+    let cont = {};
+    for (let i = 0; i < cad.length; i ++)
+    {
+        if (cont[cad[i]])
+        {
+            cont[cad[i]]++;
+        }   
+        else
+        {
+            cont[cad[i]] = 1;
+        }
+        if (cont[cad[i]] === 1)
+        {
+            console.log(cad[i]);
+        }
+    }
+}
